08e4cb63346ad7d2945bc22deac4232d9a929654
mrSkortch <jeff.szorc@gmail.com>
2015-05-18 03:03:46 -0600

    v49
    Fixed issued for flagFunc.group_alive and group_dead. Issue caused by a
    bug in DCS. Had to write minor workaround.

--- mist.lua
+++ mist.lua
@@ -8,7 +8,7 @@ mist = {}
 -- don't change these
 mist.majorVersion = 3
 mist.minorVersion = 7
-mist.build = 48
+mist.build = 49
 
 
 
@@ -3186,7 +3186,6 @@ stopFlag
 	
 	
 	if stopflag == -1 or (type(trigger.misc.getUserFlag(stopflag)) == 'number' and trigger.misc.getUserFlag(stopflag) == 0) or (type(trigger.misc.getUserFlag(stopflag)) == 'boolean' and trigger.misc.getUserFlag(stopflag) == false) then
-		if Group.getByName(groupName) and Group.getByName(groupName):isExist() == true then
 			if trigger.misc.getUserFlag(flag) == 0 then
 				trigger.action.setUserFlag(flag, true)
 			end
@@ -3223,7 +3222,6 @@ mist.flagFunc.group_dead = function(vars)
 	
 	
 	if stopflag == -1 or (type(trigger.misc.getUserFlag(stopflag)) == 'number' and trigger.misc.getUserFlag(stopflag) == 0) or (type(trigger.misc.getUserFlag(stopflag)) == 'boolean' and trigger.misc.getUserFlag(stopflag) == false) then
-		if Group.getByName(groupName) and Group.getByName(groupName):isExist() == false or not Group.getByName(groupName) then
 			if trigger.misc.getUserFlag(flag) == 0 then
 				trigger.action.setUserFlag(flag, true)
 			end
